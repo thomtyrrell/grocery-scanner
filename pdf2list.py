c93e442fab96e694bd55b31c0f872bc4970385a9
thomtyrrell <thomtyrrell@icloud.com>
2018-10-22 14:57:11 -0400

    Update pdf2list.py

--- pdf2list.py
+++ pdf2list.py
@@ -2,10 +2,10 @@ from pdf2image import convert_from_path
 import cv2
 
 
-def convert_PDF(path):
+def convert_PDF(path, store = ''):
     images = convert_from_path(path)
 
     for i in range(len(images)):
-        images[i].save('images/receipt_' + str(i) + '.png', 'PNG')
+        images[i].save('images/' + store + '_receipt_' + str(i) + '.png', 'PNG')
 
     return images
